9885fe56de7de261674ccf50612175fff996f6d7
complikatyed <katye.russell@gmail.com>
2016-05-07 10:44:30 -0500

    Refactor to use ajax instead of XHR

--- acme-explosives/main.js
+++ acme-explosives/main.js
@@ -47,32 +47,14 @@ $( "#fireworks" ).click(function() {
 // --------- First Promise (Loads from categories.json) ----------- //
 
 var loadAllCategories = function() {
-
-  return new Promise(function (resolve, reject) {
-    var categoryLoader = new XMLHttpRequest();
-
-    categoryLoader.onload = function() {
-      if(this.status === 200) {  // IF SUCCESS
-
-        var data = JSON.parse(this.responseText);
-
-        // --- 'resolve' is a variable here, the --- //
-        // --- actual function name gets passed  --- //
-        // --- in when we call the promise later --- //
-        resolve(data);
-
-      } else {  // if failure
-
-        reject(new Error(this.statusText)); // Reports back what the status was so you know what went wrong
-      }
-    }
-    categoryLoader.onerror = function() {
-      reject(new Error(
-        'XMLHTTPRequest ERROR: ' + this.statusText
-        ));
-    }
-    categoryLoader.open("GET", "jsonFiles/categories.json");
-    categoryLoader.send();
+  return new Promise((resolve, reject) => {
+    $.ajax({
+      url: "jsonFiles/categories.json"
+    }).done(function(data) {
+      resolve(data);
+    }).fail(function(xhr, status, error) {
+      reject(error);
+    });
   });
 };
 
@@ -80,61 +62,29 @@ var loadAllCategories = function() {
 // --------- Second Promise (Loads from types.json) ----------- //
 
 var loadAllTypes = function() {
-
-  return new Promise(function (resolve, reject) {  // Resolve and reject will be functions we pass in later
-    var typeLoader = new XMLHttpRequest();
-
-    typeLoader.onload = function() {
-      if(this.status === 200) {  // IF SUCCESS
-
-        var data = JSON.parse(this.responseText);
-
-        // --- 'resolve' is a variable here, the --- //
-        // --- actual function name gets passed  --- //
-        // --- in when we call the promise later --- //
-        resolve(data);
-
-      } else {
-
-        reject(new Error(this.statusText));
-      }
-    }
-    typeLoader.onerror = function() {
-      reject(new Error(
-        'XMLHTTPRequest ERROR: ' + this.statusText
-        ));
-    }
-    typeLoader.open("GET", "jsonFiles/types.json");
-    typeLoader.send();
+  return new Promise((resolve, reject) => {
+    $.ajax({
+      url: "jsonFiles/types.json"
+    }).done(function(data) {
+      resolve(data);
+    }).fail(function(xhr, status, error) {
+      reject(error);
+    });
   });
 };
+
 
 // --------- Third Promise (Loads from products.json) ----------- //
 
 var loadAllProducts = function() {
-
-  return new Promise(function (resolve, reject) {
-    var productLoader = new XMLHttpRequest();
-
-    productLoader.onload = function() {
-      if(this.status === 200) {  // IF SUCCESS
-
-        var data = JSON.parse(this.responseText);
-
-        resolve(data);
-
-      } else {
-
-        reject(new Error(this.statusText));
-      }
-    }
-    productLoader.onerror = function() {
-      reject(new Error(
-        'XMLHTTPRequest ERROR: ' + this.statusText
-        ));
-    }
-    productLoader.open("GET", "jsonFiles/products.json");
-    productLoader.send();
+  return new Promise((resolve, reject) => {
+    $.ajax({
+      url: "jsonFiles/products.json"
+    }).done(function(data) {
+      resolve(data);
+    }).fail(function(xhr, status, error) {
+      reject(error);
+    });
   });
 };
 
