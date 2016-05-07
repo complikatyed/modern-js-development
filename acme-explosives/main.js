4ef07b6bca6eba23a9975efec48fb8ac0d085d65
complikatyed <katye.russell@gmail.com>
2016-05-07 12:59:53 -0500

    Add dropdown select options dynamically

--- acme-explosives/main.js
+++ acme-explosives/main.js
@@ -1,50 +1,10 @@
 //"use strict";
-var categories = [];
-var types      = [];
-var products   = [];
 
-$( "#everything" ).click(function() {
-  loadAllCategories()
-  .then (
-    function(categoriesData) {
-      categories = categoriesData.categories;
-      return loadAllTypes();
-    },
-    function(reason) {
-      console.log("Categories didn't load properly", reason);
-  })
-  .then (
-    function(typesData){
-      types = typesData.types;
-      return loadAllProducts();
-    },
-    function(reason) {
-      console.log("Types didn't load properly", reason);
-  })
-  .then (
-    function(productsData) {
-      products = productsData.products;
-      makeProductArray(productsData);
-    },
-    function(reason) {
-      console.log("Products didn't load properly", reason);
-  });
-});
+$(document).ready(function() {
 
-// ---------- If user selects "Demolition" ---------- //
-
-$( "#demolition" ).click(function() {
-  console.log("Demolition clicked");
-});
-
-// ---------- If user selects "Fireworks" ---------- //
-
-$( "#fireworks" ).click(function() {
-  console.log("Fireworks clicked");
-});
-
-
-// --------- First Promise (Loads from categories.json) ----------- //
+  var categories = [];
+  var types      = [];
+  var products   = [];
 
 var loadAllCategories = function() {
   return new Promise((resolve, reject) => {
@@ -81,7 +41,11 @@ var loadAllProducts = function() {
     $.ajax({
       url: "jsonFiles/products.json"
     }).done(function(data) {
-      resolve(data);
+      var realProducts = data.products[0];
+      for (i in realProducts) {
+        products.push(realProducts[i]);
+      };
+      resolve(products);
     }).fail(function(xhr, status, error) {
       reject(error);
     });
@@ -89,70 +53,35 @@ var loadAllProducts = function() {
 };
 
 
-// ---------- Converts data from one big object (full of product objects)  ----------- //
-// ---------- into array of smaller objects with the keys we'll need later ----------- //
+loadAllCategories()
+  .then (
+    function(categoriesData) {
+      categories = categoriesData.categories;
+      addDropDown();
+      return loadAllTypes();
+    },
+    function(reason) {
+      console.log("Categories didn't load properly", reason);
+  })
+  .then (
+    function(typesData){
+      types = typesData.types;
+      return loadAllProducts();
+    },
+    function(reason) {
+      console.log("Types didn't load properly", reason);
+  })
+  .then (
+    function(productsData) {
+      //makeProductArray(productsData);
+      populatePage(productsData);
 
-var makeProductArray = function(data) {
-
-  var foo = data.products[0];
-
-  var productArray = [];
-
-  for(i in foo) {
-
-  var tempType = foo[i].type;
-
-      switch (tempType) {
-      case 0:
-        foo[i].type = "Personal";
-        foo[i].category = "Fireworks";
-        break;
-      case 1:
-        foo[i].type = "Wedding";
-        foo[i].category = "Fireworks";
-        break;
-      case 2:
-        foo[i].type = "Neighbors";
-        foo[i].category = "Fireworks";
-        break;
-      case 3:
-        foo[i].type = "Family";
-        foo[i].category = "Demolition";
-        break;
-      case 4:
-        foo[i].type = "Insurance";
-        foo[i].category = "Demolition";
-        break;
-      case 5:
-        foo[i].type = "Lawn and Garden";
-        foo[i].category = "Demolition";
-        break;
-    }
-
-    // --- Creates a new (smaller) object with the keys we'll need later.      --- //
-    // --- NOTE:  the new object has to be declared inside the loop or ALL the --- //
-    // ---        objects in the array will be overwritten w last item's data  --- //
-
-    var myObject = {
-      name: '',
-      description: '',
-      category: '',
-      type: ''
-    };
-
-    // --- Adds the data from the objects to our new object --- //
-    myObject.name = foo[i].name;
-    myObject.description = foo[i].description;
-    myObject.category = foo[i].category;
-    myObject.type = foo[i].type;
-
-    // --- Add the new object to the array --- //
-    productArray.push(myObject);
-
-   }
-
-   populatePage(productArray);
-};
+    },
+    function(reason) {
+      console.log("Products didn't load properly", reason);
+  });
+$( "select" ).on("change", function() {
+});
 
 
 // ---------- Function to take the items from the array and display them on page ------ //
@@ -172,3 +101,19 @@ function populatePage(productArray) {
 
   $("#products").html(productString);
 };
+
+// Function to insert select thingies dynamically
+  // Loop over the categories and use key/values:
+  // this.value will equal the category id
+  // The value should be equal to the category
+  // Name should be the name
+function addDropDown() {
+  $.each(categories, function(index, value) {
+    $('#mySelect')
+        .append($("<option></option>")
+      .attr("value", value.id)
+      .text(value.name));
+  });
+};
+
+});  // End of the document.ready function
