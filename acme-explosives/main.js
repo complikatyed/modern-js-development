d5c6e6c65572fda79a55acfff357d127d9426c8d
complikatyed <katye.russell@gmail.com>
2016-05-08 19:01:35 -0500

    Project requirements are now being met

--- acme-explosives/main.js
+++ acme-explosives/main.js
@@ -1,119 +1,162 @@
-//"use strict";
 
 $(document).ready(function() {
 
   var categories = [];
   var types      = [];
   var products   = [];
+  var fireworks  = [];
+  var demolition = [];
 
-var loadAllCategories = function() {
-  return new Promise((resolve, reject) => {
-    $.ajax({
-      url: "jsonFiles/categories.json"
-    }).done(function(data) {
-      resolve(data);
-    }).fail(function(xhr, status, error) {
-      reject(error);
+// --------- First Promise (Loads from categories.json) ----------- //
+
+  var loadAllCategories = function() {
+    return new Promise((resolve, reject) => {
+      $.ajax({
+        url: "jsonFiles/categories.json"
+      }).done(function(data) {
+        resolve(data);
+      }).fail(function(xhr, status, error) {
+        reject(error);
+      });
     });
-  });
-};
-
+  };
 
 // --------- Second Promise (Loads from types.json) ----------- //
 
-var loadAllTypes = function() {
-  return new Promise((resolve, reject) => {
-    $.ajax({
-      url: "jsonFiles/types.json"
-    }).done(function(data) {
-      resolve(data);
-    }).fail(function(xhr, status, error) {
-      reject(error);
-    });
-  });
-};
+  var loadAllTypes = function() {
+    return new Promise((resolve, reject) => {
+      $.ajax({
+        url: "jsonFiles/types.json"
+      }).done(function(data) {
 
+        var allTypes = data.types;
+        for (i in allTypes) {
+          if (allTypes[i].category === 0) {
+            allTypes[i].category = "fireworks";
+          } else {
+            allTypes[i].category = "demolition";
+          }
+          types.push(allTypes[i]);
+        };
+        resolve(data);
+      }).fail(function(xhr, status, error) {
+        reject(error);
+      });
+    });
+  };
 
 // --------- Third Promise (Loads from products.json) ----------- //
 
-var loadAllProducts = function() {
-  return new Promise((resolve, reject) => {
-    $.ajax({
-      url: "jsonFiles/products.json"
-    }).done(function(data) {
-      var realProducts = data.products[0];
-      for (i in realProducts) {
-        products.push(realProducts[i]);
-      };
-      resolve(products);
-    }).fail(function(xhr, status, error) {
-      reject(error);
+  var loadAllProducts = function() {
+    return new Promise((resolve, reject) => {
+      $.ajax({
+        url: "jsonFiles/products.json"
+      }).done(function(data) {
+        resolve(data);
+      }).fail(function(xhr, status, error) {
+        reject(error);
+      });
     });
-  });
-};
+  };
+
+// ---------- Calling the nested promises ---------- //
+
+  loadAllCategories()
+    .then (
+      function(categoriesData) {
+        categories = categoriesData.categories;
+        return loadAllTypes();
+      },
+      function(reason) {
+        console.log("Categories didn't load properly", reason);
+    })
+    .then (
+      function(typesData){
+        types = typesData.types;
+        return loadAllProducts();
+      },
+      function(reason) {
+        console.log("Types didn't load properly", reason);
+    })
+    .then (
+      function(productsData) {
+        makeProductsArray(productsData);
+      },
+      function(reason) {
+        console.log("Products didn't load properly", reason);
+    });
 
 
-loadAllCategories()
-  .then (
-    function(categoriesData) {
-      categories = categoriesData.categories;
-      addDropDown();
-      return loadAllTypes();
-    },
-    function(reason) {
-      console.log("Categories didn't load properly", reason);
-  })
-  .then (
-    function(typesData){
-      types = typesData.types;
-      return loadAllProducts();
-    },
-    function(reason) {
-      console.log("Types didn't load properly", reason);
-  })
-  .then (
-    function(productsData) {
-      //makeProductArray(productsData);
-      populatePage(productsData);
+// ----- Function to address clicked choice from catalog ----- //
 
-    },
-    function(reason) {
-      console.log("Products didn't load properly", reason);
-  });
-$( "select" ).on("change", function() {
-});
-
-
-// ---------- Function to take the items from the array and display them on page ------ //
-
-function populatePage(productArray) {
-
-  var productString = "";
-
-
-
-  $.each(productArray, function (index, value) {
-    productString += `<div class="col-sm-4 card"><h3 class="name">${value.name}</h3>`
-    productString += `<p class="description">${value.description}</p>`
-    productString += `<p class="type">Category: ${value.category}</p>`
-    productString += `<p class="type">Type: ${value.type}</p></div>`
+  $('.dropdown-menu li a').click(function(){
+    if(this.id === "fireworks") {
+      populatePage(fireworks);
+    } else if (this.id === "demolition") {
+      populatePage(demolition);
+    } else {
+      populatePage(products);
+    }
   });
 
-  $("#products").html(productString);
-};
+// ----- Function to push all products into an array ----- //
+  function makeProductsArray(data) {
+    var realProducts = data.products[0];
+    for (i in realProducts) {
+      products.push(realProducts[i]);
+    };
+    makeSortedProductsArrays(products);
+  };
 
-// Function to insert select thingies dynamically
-  // Loop over the categories and use key/values:
-  // this.value will equal the category id
-  // The value should be equal to the category
-  // Name should be the name
-function addDropDown() {
-  $.each(categories, function(index, value) {
-    $('#mySelect')
-        .append($("<option></option>")
-      .attr("value", value.id)
-      .text(value.name));
-  });
-};
 
-});  // End of the document.ready function
+// ----- Function to choose what is being added to page ----- //
+
+  function makeSortedProductsArrays(products) {
+    var sortedArray = [];
+
+    $.each(products, function(index, value) {
+
+      if (value.type < 3) {
+        value.category = "fireworks";
+        sortedArray.push(value);
+
+      } else {
+        value.category = "demolition";
+        sortedArray.push(value);
+      }
+    });
+
+    $.each(sortedArray, function(index, value) {
+      var number = value.type;
+      value.type = types[number].name;
+    });
+
+    $.each(sortedArray, function(index, value) {
+      if (value.category === "fireworks") {
+        fireworks.push(value);
+      } else
+      {
+        demolition.push(value);
+      }
+    })
+
+  };
+
+// ----- Function to take the items from the array and display them on page ------ //
+
+  function populatePage(productArray) {
+
+    var productString = "";
+
+    $.each(productArray, function (index, value) {
+      productString += `<div class="col-sm-4 card"><h3 class="name">${value.name}</h3>`
+      productString += `<p class="description">${value.description}</p>`
+      productString += `<p class="type">Category: ${value.category}</p>`
+      productString += `<p class="type">Type: ${value.type}</p></div>`
+    });
+
+    $("#products").html(productString);
+  };
+
+
+});  // ---- End of the document.ready function
